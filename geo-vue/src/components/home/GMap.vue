10e8ad3beefdea36c7831c26996f42dd4c718b8f
alex elmore <aelmore@merkleinc.com>
2022-03-29 08:56:36 -0500

    Project Cleanup, add Materialize CSS, Setup for Routing and Navbar Component

---
+++ geo-vue/src/components/home/GMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="map">
+    <div class="google-map" id="map">home</div>
+   
+  </div>
+</template>
+
+<script>
+
+import db from '@/firebase/init'
+export default {
+  name: 'GMap',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
---
+++ geo-vue/src/components/layout/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <router-link :to="{ name: '/' }" class="brand-logo left">Geo Vue</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+name:'Navbar'
+}
+</script>
+
+<style>
+
+</style>
