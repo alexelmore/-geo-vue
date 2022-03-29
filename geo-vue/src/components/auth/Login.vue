50cd9f95c82cf777a1e736e9a54b26384c0e6781
alex elmore <aelmore@merkleinc.com>
2022-03-29 11:45:59 -0500

    Setup for logging a user in from Login Component

--- geo-vue/src/components/auth/Login.vue
+++ geo-vue/src/components/auth/Login.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>login</div>
+  <div class="login container">
+    <form @submit.prevent="login" class="card-panel">
+      <h2 class="center deep-purple-text">Login</h2>
+      <div class="field">
+        <label for="email">Email</label>
+        <input id="email" type="email" v-model="email" />
+      </div>
+      <div class="field">
+        <label for="password">Password</label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn deep-purple">Login</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+// Further FB imports needed for older FB verison
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 export default {
   name: "Login",
+  data() {
+    return {
+      email: null,
+      password: null,
+      feedback: null,
+    };
+  },
+  methods: {
+    // Function to log user in on form submission
+    login() {
+      // Check if user filled out all form fields
+      if (this.email && this.password) {
+        // Set feedback property back to its default of null
+        this.feedback = null;
+        // Make call to FB to try to log user in
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email, this.password)
+          .then((user) => {
+            //console.log(user)
+            this.$router.push({ name: "GMap" });
+          })
+          .catch((err) => {
+            this.feedback = err.message;
+          });
+      } else {
+        this.feedback = "Please fill out all of the form fields";
+      }
+    },
+  },
 };
 </script>
 
--- geo-vue/src/components/layout/Navbar.vue
+++ geo-vue/src/components/layout/Navbar.vue
@@ -15,7 +15,10 @@
 </template>
 
 <script>
+// Further FB imports needed for older FB verison
 import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 export default {
   name: "Navbar",
 };
