86bf0d508e7dc7794678fea2b4e8274d305a2e89
alex elmore <aelmore@merkleinc.com>
2022-03-30 09:24:03 -0500

    Route guard for home page

--- geo-vue/src/router/index.js
+++ geo-vue/src/router/index.js
@@ -3,14 +3,21 @@ import Router from "vue-router";
 import GMap from "@/components/home/GMap";
 import Signup from "@/components/auth/Signup";
 import Login from "@/components/auth/Login";
+// Further FB imports needed for older FB verison
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: "/",
       name: "GMap",
       component: GMap,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/signup",
@@ -24,3 +31,22 @@ export default new Router({
     },
   ],
 });
+
+// Route guard for home page
+router.beforeEach((to, from, next) => {
+  // Check to see if route requires auth
+  if (to.matched.some((rec) => rec.meta.requiresAuth)) {
+    // Check if user is authenticated
+    let user = firebase.auth().currentUser;
+    if (user) {
+      // User is logged in, let them proceed to the GMap component page
+      next();
+    } else {
+      // User is not logged in or signed up, redirect them to the login page
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
